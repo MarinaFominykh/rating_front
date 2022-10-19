69eadea0e02c3ff27b4538fe14aed0915dbcb95a
Marina Fominykh <92662628+MarinaFominykh@users.noreply.github.com>
2022-10-19 15:18:18 +0300

    адрес сервера в api

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,4 +1,6 @@
-export const BASE_URL = "http://localhost:3001";
+// export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = "http://mafia-raiting.online";
+
 
 export const getMatches = () => {
     return fetch(`${BASE_URL}/matches`, {
