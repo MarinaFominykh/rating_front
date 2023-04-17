f79f2dcb240c61340e9be23a24b0fcab39d0aa37
Marina Fominykh <92662628+MarinaFominykh@users.noreply.github.com>
2023-04-17 13:46:24 +0300

    адрес сервера pilona.ru

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,6 +1,7 @@
 // export const BASE_URL = "http://localhost:3001";
 // export const BASE_URL = "https://mafia-raiting.online"; 
-export const BASE_URL = "http://1415729-cz86573.tw1.ru"; 
+// export const BASE_URL = "http://1415729-cz86573.tw1.ru"; 
+export const BASE_URL = "http://pilona.ru/api"; 
 
 // Авторизация
 export const authorize = (login, password) => {
