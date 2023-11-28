40d0c5e330e18801e3ae908afe6b3adf343196b1
Marina Fominykh <92662628+MarinaFominykh@users.noreply.github.com>
2023-11-28 15:59:07 +0300

    изменить адрес сервера

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://localhost:3001/api";
-export const BASE_URL = "https://mafia-raiting.online/api"; 
+export const BASE_URL = "https://mafia-raiting.ru/api"; 
 // export const BASE_URL = "https://api.pilona.ru"; 
 
 // Авторизация
